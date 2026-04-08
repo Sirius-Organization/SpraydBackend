1bb1ce458367a912c60abd08f83fd8aad1aa36a6
h4ssh <t.a.ross@yandex.com>
2026-04-08 15:20:12 +0300

    uncomment https

--- Sources/SpraydBack/configure.swift
+++ Sources/SpraydBack/configure.swift
@@ -7,15 +7,14 @@ import FluentPostgresDriver
 public func configure(_ app: Application) async throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-//    let certPath = "/etc/letsencrypt/live/sprayd.ru/fullchain.pem"
-//    let keyPath = "/etc/letsencrypt/live/sprayd.ru/privkey.pem"
-//
-//    app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
-//        certificateChain: try NIOSSLCertificate.fromPEMFile(certPath).map { .certificate($0) },
-//        privateKey: .privateKey(try NIOSSLPrivateKey(file: keyPath, format: .pem))
-//    )
-//
-//    app.http.server.configuration.port = 443
+    let certPath = "/etc/letsencrypt/live/sprayd.ru/fullchain.pem"
+    let keyPath = "/etc/letsencrypt/live/sprayd.ru/privkey.pem"
+
+    app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+        certificateChain: try NIOSSLCertificate.fromPEMFile(certPath).map { .certificate($0) },
+        privateKey: .privateKey(try NIOSSLPrivateKey(file: keyPath, format: .pem))
+    )
+    app.http.server.configuration.port = 443
 
     app.routes.defaultMaxBodySize = "10mb"
 
